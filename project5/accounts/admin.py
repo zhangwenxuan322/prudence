5b7c9e6059bc29e3560f01c2628a891a10a04498
zhangwenxuan322 <798043150@qq.com>
2024-09-19 19:14:54 +1000

    Add gitignore and admin page

--- project5/accounts/admin.py
+++ project5/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from prudence.models import RiskType, Control, Action, Risk
+from .models import CustomUser
+
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(RiskType)
+admin.site.register(Control)
+admin.site.register(Action)
+admin.site.register(Risk)
