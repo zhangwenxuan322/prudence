e4293e08e6e736bfa96e64b913e4baaeed0e5911
zhangwenxuan322 <798043150@qq.com>
2024-11-26 00:28:29 +1100

    Add allowed hosts

--- prudence/settings.py
+++ prudence/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-j4_f%77g&*!&5aay6ev=do%$%k$a9b2_cm4=t8089to3z1=img"
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite-bk4p.onrender.com']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
