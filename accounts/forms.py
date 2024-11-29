27ab8bac98496232be9e2ececd82f7ae500f94f2
zhangwenxuan322 <798043150@qq.com>
2024-11-29 23:28:47 +1100

    Add firstname, lastname and role in register process

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,70 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser  # Import the custom user model
 
+
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True, help_text="Required. Enter a valid email address.")
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'required': '',
+            'name': 'username',
+            'id': 'username',
+            'type': 'text',
+            'class': 'form-input',
+            'placeholder': 'Enter your username',
+        })
+
+        self.fields['first_name'].widget.attrs.update({
+            'required': '',
+            'name': 'first_name',
+            'id': 'first_name',
+            'type': 'text',
+            'class': 'form-input',
+            'placeholder': 'Enter your first name',
+        })
+
+        self.fields['last_name'].widget.attrs.update({
+            'required': '',
+            'name': 'last_name',
+            'id': 'last_name',
+            'type': 'text',
+            'class': 'form-input',
+            'placeholder': 'Enter your last name',
+        })
+
+        self.fields['email'].widget.attrs.update({
+            'required': '',
+            'name': 'email',
+            'id': 'email',
+            'type': 'text',
+            'class': 'form-input',
+            'placeholder': 'Enter a valid email address.',
+        })
+
+        self.fields['password1'].widget.attrs.update({
+            'required': '',
+            'name': 'password1',
+            'id': 'password1',
+            'type': 'password',
+            'class': 'form-input',
+            'placeholder': 'Minimum 3 characters',
+        })
+
+        self.fields['password2'].widget.attrs.update({
+            'required': '',
+            'name': 'password2',
+            'id': 'password2',
+            'type': 'password',
+            'class': 'form-input',
+            'placeholder': 'Confirm password',
+        })
 
     class Meta:
         model = CustomUser  # Reference the correct model
-        fields = ('username', 'email', 'role','password1', 'password2')
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'role', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super().save(commit=False)
--- prudence/settings.py
+++ prudence/settings.py
@@ -67,18 +67,21 @@ DATABASES = {
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    # },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 3,
+        }
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    # },
+    # {
+    #     "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    # },
 ]
 
 LANGUAGE_CODE = "en-us"
